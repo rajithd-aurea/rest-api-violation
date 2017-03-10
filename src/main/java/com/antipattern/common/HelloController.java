4622b55f60abbe74b982963e3d75701167617794
Rajith Delantha <rajithgama@gmail.com>
2017-03-10 13:17:31 +0530

    add more post end points

--- src/main/java/com/antipattern/common/HelloController.java
+++ src/main/java/com/antipattern/common/HelloController.java
@@ -1,15 +1,33 @@
 package com.antipattern.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/hello_post")
+    public void postHello() {
+        log.info("POST");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/hello_put")
+    public void putHello() {
+        log.info("PUT");
+    }
+
+    @PostMapping(value = "/hello_post_new")
+    public void postNew() {
+        log.info("POST New");
     }
 
 
