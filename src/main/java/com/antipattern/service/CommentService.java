95f0b89de488820be12e16660f9887f07be1c2d6
Rajith Delantha <rajithgama@gmail.com>
2017-03-30 12:29:28 +0530

    add service

---
+++ src/main/java/com/antipattern/service/CommentService.java
@@ -0,0 +1,34 @@
+package com.antipattern.service;
+
+/**
+ * Created by rajith on 3/30/17.
+ */
+public class CommentService {
+
+    /**
+     * test1
+     * test2
+     * test3
+     * test4
+     * test5
+     */
+    public void method1() {
+        System.out.println("aaa");
+    }
+
+    /**
+     * test6
+     * test7
+     * test8
+     * test9
+     * test10
+     */
+    public void method2() {
+        System.out.println("aaa");
+    }
+
+    public void method3() {
+        System.out.println("aaa");
+    }
+
+}
