ffb0214a18bb1a14645b73421e74e943c6aa4b69
Rajith Delantha <rajithgama@gmail.com>
2017-04-17 17:37:28 +0530

    add long method m1 and m2

---
+++ src/main/java/com/antipattern/service/LongMethodService.java
@@ -0,0 +1,108 @@
+package com.antipattern.service;
+
+/**
+ * Created by rajith on 4/17/17.
+ */
+public class LongMethodService {
+
+    /**
+     * this is m1
+     */
+    public void m1() {
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+    }
+
+    public void m2() {
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+        System.out.println("b");
+    }
+
+
+}
