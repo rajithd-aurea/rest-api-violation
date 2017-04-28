d988c936f2997d85713d171f7bb6b4466aeec936
Rajith Delantha <rajithgama@gmail.com>
2017-04-28 15:56:37 +0530

    add comment APs

--- src/main/java/com/antipattern/service/CommentService.java
+++ src/main/java/com/antipattern/service/CommentService.java
@@ -5,23 +5,17 @@ package com.antipattern.service;
  */
 public class CommentService {
 
+    private String call;
+
     /**
-     * test1
-     * test2
-     * test3
-     * test4
-     * test5
+     * test1 test2 test3 test4 test5
      */
     public void method1() {
         System.out.println("aaa");
     }
 
     /**
-     * test6
-     * test7
-     * test8
-     * test9
-     * test10
+     * test6 test7 test8 test9 test10
      */
     public void method2() {
         System.out.println("aaa");
@@ -29,6 +23,25 @@ public class CommentService {
 
     public void method3() {
         System.out.println("aaa");
+    }
+
+    public void disconnect() {
+        // Calling disconnect() before a connection exists should have no effect.
+        if (call == null) {
+            return;
+        }
+
+        new LongMethodService().m1(); // Unblock any waiting async thread.
+        call.length();
+    }
+
+    public void method4() {
+        // comment 1
+        System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa"); //comment 2
+        System.out.println("aaa"); // comment 3
+        System.out.println("aaa"); // comment 4
     }
 
 }
